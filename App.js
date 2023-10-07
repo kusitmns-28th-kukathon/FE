88224d42f53495bab46d9e6f0028ab49a42ecb34
parkyejin <uiop5809@naver.com>
2023-10-07 10:49:36 +0900

    init: 폴더 구조 생성

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import {StatusBar} from 'expo-status-bar';
-import {StyleSheet, Text, View} from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-	return (
-		<View style={styles.container}>
-			<Text>안녕하세요 큐커톤 G조 입니다!</Text>
-			<StatusBar style="auto" />
-		</View>
-	);
+  return (
+    <View style={styles.container}>
+      <Text>안녕하세요 큐커톤 G조 입니다!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
