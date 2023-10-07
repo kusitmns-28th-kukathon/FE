0544b3c6ce4fbdabe568d15b74d76d89c28a6962
parkyejin <uiop5809@naver.com>
2023-10-07 15:33:51 +0900

    feat: 캘린더 날짜 선택

--- screens/calendar.js
+++ screens/calendar.js
@@ -1,14 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { Calendar } from "react-native-calendars";
+import { format } from "date-fns";
 
 const CalendarView = () => {
   const navigation = useNavigation();
 
+  const posts = [
+    {
+      id: 1,
+      title: "제목입니다.",
+      contents: "내용입니다.",
+      date: "2023-10-26",
+    },
+    {
+      id: 2,
+      title: "제목입니다.",
+      contents: "내용입니다.",
+      date: "2023-10-27",
+    },
+  ];
+
+  const markedDates = posts.reduce((acc, current) => {
+    const formattedDate = format(new Date(current.date), "yyyy-MM-dd");
+    acc[formattedDate] = { marked: true };
+    return acc;
+  }, {});
+
+  const [selectedDate, setSelectedDate] = useState(
+    format(new Date(), "yyyy-MM-dd")
+  );
+
+  const markedSelectedDates = {
+    ...markedDates,
+    [selectedDate]: {
+      selected: true,
+      marked: markedDates[selectedDate]?.marked,
+    },
+  };
+
   return (
     <View style={Styles.container}>
-      <Calendar style={Styles.calendar} />
+      <Calendar
+        style={Styles.calendar}
+        markedDates={markedSelectedDates}
+        theme={{
+          selectedDayBackgroundColor: "#009688",
+          arrowColor: "#009688",
+          dotColor: "#009688",
+          todayTextColor: "#009688",
+        }}
+        onDayPress={(day) => {
+          setSelectedDate(day.dateString);
+        }}
+      />
     </View>
   );
 };
@@ -19,6 +65,7 @@ const Styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+    justifyContent: "center",
   },
   NextBottom: {
     backgroundColor: "purple",
@@ -34,9 +81,7 @@ const Styles = StyleSheet.create({
     textAlign: "center",
   },
   calendar: {
-    borderBottomWidth: 1,
-    borderBottomColor: "#e0e0e0",
     color: "#fff",
-    margin: 30,
+    margin: 20,
   },
 });
