b9743e2454e2dcb7a4e7750c7c29a21118b45373
hoyyChoi <hoyeon8165@gmail.com>
2023-10-08 03:30:31 +0900

    fix : load time

--- screens/splash.js
+++ screens/splash.js
@@ -1,33 +1,33 @@
-import React, { useEffect } from "react";
-import { View, Text, StyleSheet, Image } from "react-native";
-import { useNavigation } from "@react-navigation/native";
+import React, {useEffect} from 'react';
+import {View, Text, StyleSheet, Image} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
 
 const Splash = () => {
-  const navigation = useNavigation();
+	const navigation = useNavigation();
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      navigation.navigate("Login");
-    }, 4500);
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			navigation.navigate('Login');
+		}, 2000);
 
-    return () => clearTimeout(timer);
-  }, []);
+		return () => clearTimeout(timer);
+	}, []);
 
-  return (
-    <View style={Styles.container}>
-      <Image
-        source={require("../assets/splash.gif")}
-        style={{ width: "100%", height: "100%" }}
-      />
-    </View>
-  );
+	return (
+		<View style={Styles.container}>
+			<Image
+				source={require('../assets/splash.gif')}
+				style={{width: '100%', height: '100%'}}
+			/>
+		</View>
+	);
 };
 
 export default Splash;
 
 const Styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+	},
 });
