bc931e5679a46febb50444bde2fd6b94abc4602d
parkyejin <uiop5809@naver.com>
2023-10-08 02:04:37 +0900

    feat: 스플래쉬 구현

--- screens/splash.js
+++ screens/splash.js
@@ -1,45 +1,33 @@
-import React from 'react';
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
-import {useNavigation} from '@react-navigation/native';
+import React, { useEffect } from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 const Splash = () => {
-	const navigation = useNavigation();
+  const navigation = useNavigation();
 
-	return (
-		<View style={Styles.container}>
-			<Text style={Styles.HomeText}>스플래시 화면</Text>
-			<TouchableOpacity
-				onPress={() => navigation.navigate('Login', {screen: 'Login'})}
-				style={Styles.NextBottom}
-			>
-				<Text style={Styles.BottomText}>카카오 로그인 화면으로</Text>
-			</TouchableOpacity>
-		</View>
-	);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigation.navigate("Login");
+    }, 6000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <View style={Styles.container}>
+      <Image
+        source={require("../assets/splash.gif")}
+        style={{ width: "100%", height: "100%" }}
+      />
+    </View>
+  );
 };
 
 export default Splash;
 
 const Styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-	},
-	HomeText: {
-		fontSize: 30,
-		textAlign: 'center',
-	},
-	NextBottom: {
-		backgroundColor: 'purple',
-		padding: 10,
-		marginTop: '20%',
-		width: '50%',
-		alignSelf: 'center',
-		borderRadius: 10,
-	},
-	BottomText: {
-		fontSize: 15,
-		color: 'white',
-		textAlign: 'center',
-	},
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
 });
