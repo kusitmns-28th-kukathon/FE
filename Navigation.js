b3cd66e7498005ee46eb78120929810b133b3176
parkyejin <uiop5809@naver.com>
2023-10-07 18:06:11 +0900

    feat: tabBar icon 추가

--- Navigation.js
+++ Navigation.js
@@ -2,6 +2,7 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
+import { Image } from "react-native";
 
 import Home from "./screens/home";
 import Login from "./screens/Login";
@@ -13,36 +14,79 @@ import Calendar from "./screens/calendar";
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
+const activeHomeIcon = require("./assets/navigation/home1.png");
+const inactiveHomeIcon = require("./assets/navigation/home.png");
+
+const activeCalendarIcon = require("./assets/navigation/calendar1.png");
+const inactiveCalendarIcon = require("./assets/navigation/calendar.png");
+
+const activeFriendIcon = require("./assets/navigation/friend1.png");
+const inactiveFriendIcon = require("./assets/navigation/friend.png");
+
+const activeMyIcon = require("./assets/navigation/my1.png");
+const inactiveMyIcon = require("./assets/navigation/my.png");
+
 function TabNavigator() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      tabBarOptions={{
+        labelStyle: {
+          color: "black", // 활성화된 탭의 폰트 색상
+        },
+        inactiveTintColor: "gray", // 비활성화된 탭의 폰트 색상
+      }}
+    >
       <Tab.Screen
         name="홈"
         component={Home}
-        options={{
+        options={({ route }) => ({
+          tabBarIcon: ({ focused }) => (
+            <Image
+              source={focused ? activeHomeIcon : inactiveHomeIcon}
+              style={{ width: 24, height: 24 }}
+            />
+          ),
           headerShown: false,
-        }}
+        })}
       />
       <Tab.Screen
         name="캘린더"
         component={Calendar}
-        options={{
+        options={({ route }) => ({
+          tabBarIcon: ({ focused }) => (
+            <Image
+              source={focused ? activeCalendarIcon : inactiveCalendarIcon}
+              style={{ width: 24, height: 24 }}
+            />
+          ),
           headerShown: false,
-        }}
+        })}
       />
       <Tab.Screen
         name="친구"
         component={Friend}
-        options={{
+        options={({ route }) => ({
+          tabBarIcon: ({ focused }) => (
+            <Image
+              source={focused ? activeFriendIcon : inactiveFriendIcon}
+              style={{ width: 24, height: 24 }}
+            />
+          ),
           headerShown: false,
-        }}
+        })}
       />
       <Tab.Screen
         name="마이"
         component={MyPage}
-        options={{
+        options={({ route }) => ({
+          tabBarIcon: ({ focused }) => (
+            <Image
+              source={focused ? activeMyIcon : inactiveMyIcon}
+              style={{ width: 24, height: 24 }}
+            />
+          ),
           headerShown: false,
-        }}
+        })}
       />
     </Tab.Navigator>
   );
